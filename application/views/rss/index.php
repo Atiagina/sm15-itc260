ecf462bfba1d2af7d915ac7e0f04421f37ff0b47
Anna <annaatiagina@Annas-MacBook-Pro.local>
2015-07-22 22:07:52 -0700

    RSS feed incorporated into CI app
    Rss/index view, Rss_model and Rss controller to see Cats and Musicians
    Google feed

---
+++ application/views/rss/index.php
@@ -0,0 +1,18 @@
+<?php
+//views/rss/view.php
+$this->load->view($this->config->item('theme') . 'header');
+?>
+
+<?php foreach ($rss->channel->item as $rss_item): ?>
+    <h3><?php echo $rss_item->title ?></h3>
+    <div>
+        <?php echo $rss_item->description ?>
+    </div>
+    <p><a href="<?php echo $rss_item->link ?>">View article</a></p>
+<?php endforeach ?>
+
+</div><!-- end of #main -->
+
+   <?php
+    $this->load->view($this->config->item('theme') . 'footer');
+?>
